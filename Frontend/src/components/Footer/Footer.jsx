6ff2a43b308eeff447297a1f7caa24bf199f03f2
sakethram113 <rampodicheti1234@gmail.com>
2023-07-27 16:55:40 +0530

    section completed

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,142 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../../assets/images/logo.png'
+import {RiLinkedinFill} from 'react-icons/ri'
+import {AiFillYoutube, AiFillGithub, AiOutlineInstagram} from 'react-icons/ai'
+
+const socialLinks = [
+  {
+    path: "https://www.youtube.com/channel/UCUUiWO66XsPY7AQe13B9Nww",
+    icon: <AiFillYoutube className='group-hover: text-white w-4 h-5'/>,
+  },
+  {
+    path: "https://www.github.com/sakethram113",
+    icon: <AiFillGithub className='group-hover: text-white w-4 h-5'/>,
+  },
+  {
+    path: "https://www.instagram.com/sakethram",
+    icon: <AiOutlineInstagram className='group-hover: text-white w-4 h-5'/>,
+  },
+  {
+    path: "https://www.linkedin.com/in/saketh-ram-podicheti-113423247/",
+    icon: <RiLinkedinFill className='group-hover: text-white w-4 h-5'/>,
+  },
+]
+const quickLinks = [
+  {
+    path: "/home",
+    display: 'Home',
+  },
+  {
+    path: "/",
+    display: 'About us',
+  },
+  {
+    path: "/services",
+    display: 'Services',
+  },
+  {
+    path: "/",
+    display: 'Blog',
+  },
+  
+]
+const quickLinks02 = [
+  {
+    path: "/find-a-doctor",
+    display: 'Find a Doctor',
+  },
+  {
+    path: "/",
+    display: 'Request an appointment',
+  },
+  {
+    path: "/",
+    display: 'Find a location',
+  },
+  {
+    path: "/",
+    display: 'Get an opinion',
+  },
+  
+]
+const quickLinks03 = [
+  {
+    path: "/",
+    display: 'Donate',
+  },
+  {
+    path: "/contact",
+    display: 'Contact Us',
+  },
+]
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+
+  const year = new Date().getFullYear();
+  return ( 
+    <footer className='pb-16 pt-10'>
+    <div className="container">
+      <div className="flex justify-between flex-col md:flex-row flex-wrap gap-[30px]">
+        <div>
+          <img src={logo} alt="" />
+          <p className='text-[16px] leading-7 font-[400] text-textColor mt-4'>
+            Copyright &copy; {year} developed by Saketh Ram Podicheti <br /> All rights reserved.
+          </p>
+
+          <div className='flex items-center gap-3 mt-4'>
+            {socialLinks.map((link, index)=> <Link to={link.path} key={index} className='w-9 h-9 border border-solid bg-black border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+              {link.icon}
+              </Link> 
+            )}
+          </div>
+        </div>
+        
+        <div>
+          <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
+            Quick Links
+          </h2>
+
+          <ul>
+            {quickLinks.map((item,index)=> <li key={index} className='mb-4'>
+              <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>
+                {item.display}
+              </Link>
+            </li>)}
+          </ul>
+        </div>
+        <div>
+          <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
+            I Want to:
+          </h2>
+
+          <ul>
+            {quickLinks02.map((item,index)=> <li key={index} className='mb-4'>
+              <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>
+                {item.display}
+              </Link>
+            </li>)}
+          </ul>
+        </div>
+        <div>
+          <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
+            Support Us
+          </h2>
+
+          <ul>
+            {quickLinks03.map((item,index)=> <li key={index} className='mb-4'>
+              <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>
+                {item.display}
+              </Link>
+            </li>)}
+          </ul>
+        </div>
+
+
+      </div>
+    </div>
+  </footer>
+  );
+};
 
 export default Footer
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -15,6 +15,7 @@ import About from '../components/About/About';
 import ServiceList from '../components/Services/ServiceList';
 import DoctorList from '../components/Doctors/DoctorList';
 import FaqList from '../components/Faq/FaqList';
+import Testimonial from '../components/Testimonial/Testimonial';
 
 const Home = () => {
   return(
@@ -242,6 +243,20 @@ const Home = () => {
             </div>
            </section>
            {/* ----------------faqs ends----------------- */}
+           {/* ----------------Testimonial start----------------- */}
+           <section>
+            <div className="container">
+            <div className='lg:w-[470px] mx-auto'>
+                  <h2 className='heading text-center'>What Our Patients Say</h2>
+                  <p className='text__para text-center'>
+                    World class Health-Care for everyone. Our health system offers unmatched, expert health care
+                  </p>
+              </div>
+
+              <Testimonial/>
+            </div>
+           </section>
+           {/* ----------------Testimonial ends----------------- */}
       </>
   );  
   
