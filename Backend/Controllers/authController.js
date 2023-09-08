9f323052572da20af1560cf1f996bb2be8f8b999
sakethram113 <rampodicheti1234@gmail.com>
2023-09-08 10:35:02 +0530

    checking login and register credentials

--- Backend/Controllers/authController.js
+++ Backend/Controllers/authController.js
@@ -3,57 +3,66 @@ import Doctor from '../models/DoctorSchema.js'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 
-export const register = async(req, res) => {
+const generateToken = user =>{
+    return jwt.sign({id:user._id, role: user.role}, process.env.JWT_SECRET_KEY,{
+        expiresIn:'75d',
+    })
+}
+
+export const register = async(req, res) => {    
 
     const {email, password, name, role, photo, gender} = req.body
     try {
         let user = null
 
         if(role === 'patient'){
-            user = await User.findOne({email})
+            user = await User.findOne({email});
         }
         else if(role === 'doctor'){
-            user = await Doctor.findOne({email})
+            user = await Doctor.findOne({email});
         }
         // check if user exist
         if(user){
-            return res.status(400).json({message:'User already exists'})
+            return res.status(400).json({message:'User already exists'});
         }
         // hash password
 
-        const salt = await bcrypt.genSalt(10)
-        const hashPassword = await bcrypt.hash(password, salt)
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
 
         if(role === 'patient'){
-            user = newUser({
+            user = new User({
                 name,
                 email,
                 password: hashPassword,
                 photo,
                 gender,
                 role
-            })
+            });
         }
 
         if(role === 'doctor'){
-            user = newDoctor({
+            user = new Doctor({
                 name,
                 email,
                 password: hashPassword,
                 photo,
                 gender,
                 role
-            })
+            });
         }
 
-        await user.save()
+        await user.save();
 
-        res.status(200).json({
+        res
+        .status(200)
+        .json({
             success:true,
             message:'User created successfully'
         })
 
     } catch (err) {
+        console.error(err);
         res.status(500).json({
             success:false,
             message:'Server error, Try again!'
@@ -61,9 +70,55 @@ export const register = async(req, res) => {
     }
 }
 export const login = async(req, res) => {
+
+    const {email} = req.body
     try {
+
+        let user = null;
+
+        const patient = await User.findOne({email})
+        const doctor = await Doctor.findOne({email})
+
+        if(patient){
+            user = patient
+        }
+        if(doctor){
+            user = doctor
+        }
+
+        // if user doesnt exist
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        // password match check
+        const isPasswordMatch = await bcrypt.compare(req.body.password, user.password)
+
+        if(!isPasswordMatch){
+            return res.status(400).json({
+                status: false,
+                message: 'Invalid Credentials'
+            })
+        }
+
+        // get token
+
+        const token = generateToken(user)
+
+        const {paswword, role, appointments, ...rest} = user._doc
+
+        res.status(200).json({
+            status: true,
+            message: 'Login Successfull', 
+            token,
+            data:{...rest},
+            role,
+        })
         
+
     } catch (err) {
-        
+        res.status(500).json({
+            status: false,
+            message: 'Unable to login'
+        })
     }
 }
