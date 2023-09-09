0fca2050d70e0a1a09d16a84a00e5baec9d5f788
sakethram113 <rampodicheti1234@gmail.com>
2023-09-09 16:21:23 +0530

    authorizing review system

--- Backend/auth/verifyToken.js
+++ Backend/auth/verifyToken.js
@@ -19,8 +19,10 @@ export const authenticate = async(req, res, next) => {
         
         // verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY) 
+
         req.userId = decoded.id 
         req.role = decoded.role
+
         next(); //must be calling the next next funxtion
     }catch(err){
         if(err.name === 'TokenExpiredError'){
@@ -31,29 +33,35 @@ export const authenticate = async(req, res, next) => {
     }
 }
 
-export const restrict = roles => async(res, req, next) => {
-    const userId = req.userId
+export const restrict = roles => async (req, res, next) => {
+    const userId = req.userId;
 
     let user;
 
-    const patient = await User.findById(userId)
-    const doctor = await Doctor.findById(userId)
+    try {
+        const patient = await User.findById(userId);
+        const doctor = await Doctor.findById(userId);
 
-    if(patient){
-        user = patient
+        if (patient) {
+            user = patient;
+        }
+        if (doctor) {
+            user = doctor;
+        }
+
+        if (!user || !user.role) {
+            return res.status(401).json({ success: false, message: 'User data is missing or invalid' });
+        }
+
+        if (!roles.includes(user.role)) {
+            return res.status(401).json({ success: false, message: 'You are not authorized' });
+        }
+
+        next();
+    } catch (error) {
+        return res.status(500).json({ success: false, message: 'Internal server error' });
     }
-    if(doctor){
-        user = doctor
-    }
+};
 
-    if(!roles.includes(user.role)){
-
-        return res.status(401).json({success:false, message: 'You are not authorized'})
-
-    }
-
-    next();
-
-}
 
 
