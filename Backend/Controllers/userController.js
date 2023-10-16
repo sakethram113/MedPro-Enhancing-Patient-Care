f9dcaffdca4e59b3ac7cea4142afd2eef3d70b9e
sakethram113 <rampodicheti1234@gmail.com>
2023-10-16 21:46:11 +0530

    getting the user profile boilerplate

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -73,3 +73,20 @@ export const getAllUser = async(req,res)=>{
         })
     }
 }
+
+export const getUserProfile = async(req,res) => {
+    const userId = req.userId
+
+    try {
+        const user = await User.findById(userId)
+
+        if(!user){
+            return res.status(404).json({success:false, message: 'User not found'})
+        }
+
+        const {password, ...rest} = user._doc
+        res.status(200).json({success:true, message:'Profile info is getting', data:{...rest}})
+    } catch (err) {
+        res.status(404).json({success:false, message: 'User not found'})
+    }
+}
