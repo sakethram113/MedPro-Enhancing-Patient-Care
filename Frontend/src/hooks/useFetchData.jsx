2ce369b17fe1bbcc9a7159347541861563e1349b
sakethram113 <rampodicheti1234@gmail.com>
2023-10-23 10:15:26 +0530

    updating fetching data component

--- Frontend/src/hooks/useFetchData.jsx
+++ Frontend/src/hooks/useFetchData.jsx
@@ -5,6 +5,17 @@ const useFetchData = (url) => {
 
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    useEffect(()=>{
+        const fetchData = async ()=>{
+            const res = await fetch(url, {
+                headers:{Authorization : `Bearer ${token}`}
+            })
+        } 
+    }, [])
+
+
   return (
     <div>useFetchData</div>
   )
