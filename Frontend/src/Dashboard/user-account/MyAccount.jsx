9bb6085faea02d93b1924912ce584f0e98d3467a
sakethram113 <rampodicheti1234@gmail.com>
2023-10-23 10:11:32 +0530

    fetching data component

--- Frontend/src/Dashboard/user-account/MyAccount.jsx
+++ Frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -16,7 +16,8 @@ const MyAccount = () => {
   }
 
   return (
-  <div className='max-w-[1170px] px-5 mx-auto'>
+  <section>
+    <div className='max-w-[1170px] px-5 mx-auto'>
     <div className="grid md:grid-cols-3 gap-10">
       <div className="pb-[50px] px-[30px] rounded-md">
         <div className="flex items-center justify-center">
@@ -63,6 +64,7 @@ const MyAccount = () => {
       </div>
     </div>
   </div>
+  </section>
   )
   
 }
---
+++ Frontend/src/hooks/useFetchData.jsx
@@ -0,0 +1,13 @@
+import {useEffect, useState} from 'react'
+
+const useFetchData = (url) => {
+
+
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(false)
+  return (
+    <div>useFetchData</div>
+  )
+}
+
+export default useFetchData
