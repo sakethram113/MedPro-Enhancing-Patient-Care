6e25d006c3f18a91d67bbf238dc57620703517e6
sakethram113 <rampodicheti1234@gmail.com>
2023-10-27 21:44:09 +0530

    mybooking initialized

--- Frontend/src/Dashboard/user-account/MyBookings.jsx
+++ Frontend/src/Dashboard/user-account/MyBookings.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import useFetchData from "../../hooks/useFetchData"
+import { BASE_URL } from "../../config"
+import DoctorCard from '../../components/Doctors/DoctorCard'
+import Loading from "../../components/Loader/Loading"
+import Error from "../../components/Error/Error"
 
 const MyBookings = () => {
+
+  const {data:appointments, loading, error} = useFetchData(`${BASE_URL}/users/appointments/my-appointments`)
   return (
-    <div>MyBookings</div>
+    <div>
+      
+        {loading && !error && <Loading/>}
+
+        {error && !loading && <Error errMessage={error}/>}
+      
+    </div>
   )
 }
 
