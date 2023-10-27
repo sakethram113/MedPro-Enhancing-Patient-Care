377699643bbc35c50b53297283ed4acd4299ccaf
sakethram113 <rampodicheti1234@gmail.com>
2023-10-27 21:23:31 +0530

    myaccount i9nterface updation

--- Frontend/src/Dashboard/user-account/MyAccount.jsx
+++ Frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -24,7 +24,8 @@ const MyAccount = () => {
   return (
   <section>
     <div className='max-w-[1170px] px-5 mx-auto'>
-    <div className="grid md:grid-cols-3 gap-10">
+    {
+      !loading && !error && <div className="grid md:grid-cols-3 gap-10">
       <div className="pb-[50px] px-[30px] rounded-md">
         <div className="flex items-center justify-center">
           <figure className='w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor'>
@@ -69,6 +70,7 @@ const MyAccount = () => {
 
       </div>
     </div>
+    }
   </div>
   </section>
   )
